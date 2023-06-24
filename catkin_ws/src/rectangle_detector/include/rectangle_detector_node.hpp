a4740d1e7824fbfa244807a0f2b7867a46ebb902
Neathle <gervemar@hotmail.es>
2023-06-24 17:53:19 +0000

    working detector with intersections

--- catkin_ws/src/rectangle_detector/include/rectangle_detector_node.hpp
+++ catkin_ws/src/rectangle_detector/include/rectangle_detector_node.hpp
@@ -5,4 +5,19 @@
 #include <sensor_msgs/CameraInfo.h>
 #include <cv_bridge/cv_bridge.h>
 
+ros::Publisher im_pub_lines;
+ros::Publisher im_pub_rectangles;
+ros::Publisher im_pub_graph;
+cv::RNG rng(0xFFFFFFFF);
+
+
 void elongate_lines(std::vector<cv::Vec4f>& lines, float factor);
+bool isWhite(cv::Mat &img, cv::Point2f p1, cv::Point2f p2);
+std::vector<cv::Vec4f> extractLines(cv::Mat &img);
+bool intersect(cv::Vec4f line1, cv::Vec4f line2, cv::Point2i &int_pt);
+std::vector<cv::Point2i> nonMaximumSuppression(const std::vector<cv::Point2i>& intersections, float threshold);
+std::vector<cv::Point2i> findIntersections(std::vector<cv::Vec4f> &lines);
+std::vector<std::vector<bool>> createGraph(cv::Mat &linesImg, std::vector<cv::Point2i> &intersections);
+int trapezoid_area(cv::Point2i a, cv::Point2i b, cv::Point2i c, cv::Point2i d);
+std::vector<cv::Vec8i> findTrapezoids(const std::vector<cv::Point2i>& intersections, const std::vector<std::vector<bool>>& adjacencyMatrix, int minArea, int maxArea);
+cv::Mat reduceSaturationBrightness(const cv::Mat& image, float saturationScale, float valueScale);
--- catkin_ws/src/rectangle_detector/src/rectangle_detector_node.cpp
+++ catkin_ws/src/rectangle_detector/src/rectangle_detector_node.cpp
@@ -4,11 +4,8 @@
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <cv_bridge/cv_bridge.h>
+#include <numeric>
 #include "rectangle_detector_node.hpp"
-
-ros::Publisher im_pub_lines;
-ros::Publisher im_pub_rectangles;
-ros::Publisher im_pub_contours;
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -20,74 +17,73 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     cv::Mat gray;
     cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
 
-    // Create an instance of the FastLineDetector class
-    cv::Ptr<cv::ximgproc::FastLineDetector> lsd = cv::ximgproc::createFastLineDetector();
+    // Extract lines from the image using FLD
+    std::vector<cv::Vec4f> lines = extractLines(gray);
+    ROS_INFO("Extracted %lu lines", lines.size());
 
-    // Detect the lines
-    std::vector<cv::Vec4f> lines;
-    cv::Mat mask = cv::Mat::ones( image.size(), CV_8UC1 );
-    lsd->detect(gray, lines);
-    elongate_lines(lines, 1.1);
-    
-    ROS_INFO("Found %lu lines", lines.size());
+    // Create a black image
+    cv::Mat linesImg = cv::Mat::zeros(gray.size(), CV_8UC1);
 
-    cv::Mat cdst = cv::Mat::zeros(image.size(), CV_8UC3);
-    cv::RNG rng(12345);
-    cv::Scalar color = cv::Scalar(255);
-    for( size_t i = 0; i < lines.size(); i++ )
+    // Paint the lines on the black image in random colors
+    cv::Scalar color(255);
+    for (const cv::Vec4f &line : lines)
     {
-        cv::Vec4f l = lines[i];
-        cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        cv::line( cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), color, 1, cv::LINE_AA);
-    }
-    // cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(7,7));
-    // cv::morphologyEx(cdst, cdst, cv::MORPH_CLOSE, kernel);
-
-    // kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
-    // cv::erode(cdst, cdst, kernel);
-
-    // Find polygons in the cdst image
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(cdst, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-    ROS_INFO("Found %lu contours", contours.size());
-
-    // Draw contours
-    cv::Mat drawing = cv::Mat::zeros(cdst.size(), CV_8UC3);
-    for (size_t i = 0; i < contours.size(); i++)
-    {
-        cv::Scalar color = cv::Scalar(255, 255, 255);
-        cv::drawContours(drawing, contours, (int)i, color, 1, cv::LINE_AA);
+        cv::line(linesImg, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), color, 2, cv::LINE_AA);
     }
 
+    // Find all the intersection points between the lines
+    std::vector<cv::Point2i> intersections = findIntersections(lines);
+    ROS_INFO("Found %lu intersections", intersections.size());
 
-    // Find polygons with four corners
-    std::vector<std::vector<cv::Point>> polygons;
-    int cdst_area = cdst.rows * cdst.cols;
-    for (const auto& contour : contours)
+    // Draw the intersection points on the image
+    for (const cv::Point2i &intersection : intersections)
     {
-        std::vector<cv::Point> approx;
-        cv::approxPolyDP(contour, approx, cv::arcLength(contour, true) * 0.1, true);
-        if (approx.size() == 4 && cv::isContourConvex(approx) && cv::contourArea(approx)/cdst_area > 0.001 && cv::contourArea(approx)/cdst_area < 0.4)
-            polygons.push_back(approx);
+        cv::circle(linesImg, intersection, 2, cv::Scalar(255), 2);
     }
 
-    // Draw polygons
-    for (const auto& polygon : polygons)
-    {
-        // Draw corners
-        for (const auto& point : polygon)
-            cv::circle(image, point, 3, cv::Scalar(0, 255, 0), -1);
+    // Create a graph where nodes are intersection points and edges are lines that join them
+    std::vector<std::vector<bool>> graph = createGraph(linesImg, intersections);
 
-        // Draw edges
-        for (size_t i = 0; i < polygon.size(); i++)
-            cv::line(image, polygon[i], polygon[(i + 1) % polygon.size()], cv::Scalar(0, 255, 0), 1);
+    // Create a black image
+    cv::Mat graphImg = cv::Mat::zeros(gray.size(), CV_8UC3);
+
+    // Draw valid lines
+    for (int i = 0; i < graph.size(); i++)
+    {
+        for (int j = i + 1; j < graph.size(); j++)
+        {
+            if (graph[i][j])
+            {
+                cv::line(graphImg, intersections[i], intersections[j], cv::Scalar(0, 255, 0), 1, cv::LINE_AA);
+            }
+        }
+    }
+    for (const cv::Point2i &intersection : intersections)
+    {
+        cv::circle(graphImg, intersection, 2, cv::Scalar(255, 255, 255), 2);
+    }
+
+    // Extract all possible trapezoids from the graph
+
+    std::vector<cv::Vec8i> trapezoids = findTrapezoids(intersections, graph, 0.001 * gray.rows * gray.cols, 0.3 * gray.rows * gray.cols);
+    ROS_INFO("Found %lu trapezoids", trapezoids.size());
+
+    // Draw the trapezoids on the image
+    image = reduceSaturationBrightness(image, 0.5f, 0.5f);
+    for (const auto &trapezoid : trapezoids)
+    {
+        color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        cv::line(image, cv::Point(trapezoid[0], trapezoid[1]), cv::Point(trapezoid[2], trapezoid[3]), color, 1, cv::LINE_AA);
+        cv::line(image, cv::Point(trapezoid[2], trapezoid[3]), cv::Point(trapezoid[4], trapezoid[5]), color, 1, cv::LINE_AA);
+        cv::line(image, cv::Point(trapezoid[4], trapezoid[5]), cv::Point(trapezoid[6], trapezoid[7]), color, 1, cv::LINE_AA);
+        cv::line(image, cv::Point(trapezoid[6], trapezoid[7]), cv::Point(trapezoid[0], trapezoid[1]), color, 1, cv::LINE_AA);
     }
 
     // Publish images
     cv_bridge::CvImage cv_image_lines;
     cv_image_lines.header = msg->header;
-    cv_image_lines.encoding = sensor_msgs::image_encodings::BGR8;
-    cv_image_lines.image = cdst;
+    cv_image_lines.encoding = sensor_msgs::image_encodings::MONO8;
+    cv_image_lines.image = linesImg;
     im_pub_lines.publish(cv_image_lines.toImageMsg());
 
     cv_bridge::CvImage cv_image_rectangles;
@@ -96,11 +92,11 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     cv_image_rectangles.image = image;
     im_pub_rectangles.publish(cv_image_rectangles.toImageMsg());
 
-    cv_bridge::CvImage cv_image_contours;
-    cv_image_contours.header = msg->header;
-    cv_image_contours.encoding = sensor_msgs::image_encodings::BGR8;
-    cv_image_contours.image = drawing;
-    im_pub_contours.publish(cv_image_contours.toImageMsg());
+    cv_bridge::CvImage cv_image_graph;
+    cv_image_graph.header = msg->header;
+    cv_image_graph.encoding = sensor_msgs::image_encodings::BGR8;
+    cv_image_graph.image = graphImg;
+    im_pub_graph.publish(cv_image_graph.toImageMsg());
 
 }
 
@@ -112,7 +108,7 @@ int main(int argc, char** argv)
     ros::Subscriber im_sub = nh.subscribe("/image_publisher/image_raw", 1, imageCallback);
     im_pub_lines = nh.advertise<sensor_msgs::Image>("/rectangle_detector/lines", 1);
     im_pub_rectangles = nh.advertise<sensor_msgs::Image>("/rectangle_detector/rectangles", 1);
-    im_pub_contours = nh.advertise<sensor_msgs::Image>("/rectangle_detector/contours", 1);
+    im_pub_graph = nh.advertise<sensor_msgs::Image>("/rectangle_detector/graph", 1);
 
     ros::spin();
 }
@@ -136,4 +132,174 @@ void elongate_lines(std::vector<cv::Vec4f>& lines, float factor)
         line[2] = x2 + dx * extension;
         line[3] = y2 + dy * extension;
     }
+}
+
+// Function to check if a line is sufficiently white
+bool isWhite(cv::Mat &img, cv::Point2i p1, cv::Point2i p2)
+{
+    cv::LineIterator it(img, p1, p2);
+    int whiteCount = 0;
+    for (int i = 0; i < it.count; i++, ++it)
+    {
+        if (img.at<uchar>(it.pos()) > 0)
+        {
+            whiteCount++;
+        }
+    }
+    return (whiteCount >= it.count * 0.95);
+}
+
+// Function to extract lines from an image using FLD
+std::vector<cv::Vec4f> extractLines(cv::Mat &img)
+{
+    // Create a black image
+    cv::Mat linesImg = cv::Mat::zeros(img.size(), CV_8UC1);
+
+    // Extract lines from the image using FLD
+    cv::Ptr<cv::ximgproc::FastLineDetector> fld = cv::ximgproc::createFastLineDetector();
+    std::vector<cv::Vec4f> lines;
+    fld->detect(img, lines);
+
+    // Paint the lines on the black image
+    for (const cv::Vec4f &line : lines)
+    {
+        cv::line(linesImg, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), cv::Scalar(255), 1);
+    }
+
+    elongate_lines(lines, 1.2);
+
+    return lines;
+}
+
+// Function to check if two line segments intersect
+bool intersect(cv::Vec4f line1, cv::Vec4f line2, cv::Point2i &int_pt)
+{
+    cv::Point2f p = cv::Point2f(line1[0], line1[1]);
+    cv::Point2f q = cv::Point2f(line2[0], line2[1]);
+    cv::Point2f s = cv::Point2f(line2[2] - line2[0], line2[3] - line2[1]);
+    cv::Point2f r = cv::Point2f(line1[2] - line1[0], line1[3] - line1[1]);
+
+    float rxs = r.x * s.y - r.y * s.x;
+    float qpxr = (q.x - p.x) * r.y - (q.y - p.y) * r.x;
+    if (rxs == 0)
+        return false;
+    
+    float t = ((q.x - p.x) * s.y - (q.y - p.y) * s.x) / rxs;
+    float u = ((q.x - p.x) * r.y - (q.y - p.y) * r.x) / rxs;
+
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1)
+    {
+        int_pt.x = std::round(p.x + t * r.x);
+        int_pt.y = std::round(p.y + t * r.y);
+        return true;
+    }
+
+    return false;
+}
+
+std::vector<cv::Point2i> nonMaximumSuppression(const std::vector<cv::Point2i>& intersections, float threshold) 
+{
+    std::vector<cv::Point2i> filteredIntersections;
+    std::vector<bool> suppressed(intersections.size(), false);
+    for (int i = 0; i < intersections.size(); ++i) {
+        if (suppressed[i]) continue;
+
+        filteredIntersections.push_back(intersections[i]);
+        for (int j = i + 1; j < intersections.size(); ++j) {
+            if (cv::norm(intersections[i] - intersections[j]) < threshold) {
+                suppressed[j] = true;
+            }
+        }
+    }
+    return filteredIntersections;
+}
+
+// Function to find all the intersection points between a set of lines
+std::vector<cv::Point2i> findIntersections(std::vector<cv::Vec4f> &lines)
+{
+    std::vector<cv::Point2i> intersections;
+    for (size_t i = 0; i < lines.size(); i++)
+    {
+        for (size_t j = i + 1; j < lines.size(); j++)
+        {
+            cv::Point2i intersection;
+            if (intersect(lines[i], lines[j], intersection))
+                intersections.push_back(intersection);
+        }
+    }
+
+    return nonMaximumSuppression(intersections, 1);
+}
+
+// Function to create a graph where nodes are intersection points and edges are lines that join them
+std::vector<std::vector<bool>> createGraph(cv::Mat &linesImg, std::vector<cv::Point2i> &intersections)
+{
+    std::vector<std::vector<bool>> adjacencyMatrix(intersections.size(), std::vector<bool>(intersections.size(), false));
+    for (int i = 0; i < intersections.size(); ++i) {
+        for (int j = i + 1; j < intersections.size(); ++j) {
+            if (isWhite(linesImg, intersections[i], intersections[j])) {
+                adjacencyMatrix[i][j] = true;
+                adjacencyMatrix[j][i] = true;
+            }
+        }
+    }
+
+    //Number of valid lines
+    ROS_INFO("Number of valid lines: %d", 
+        std::accumulate(adjacencyMatrix.begin(), adjacencyMatrix.end(), 0, [](int sum, const std::vector<bool>& v) {
+            return sum + std::accumulate(v.begin(), v.end(), 0);
+        }) / 2
+    );
+    return adjacencyMatrix;
+}
+
+// Function to calculate the area of a trapezoid
+int trapezoid_area(cv::Point2i a, cv::Point2i b, cv::Point2i c, cv::Point2i d) 
+{
+    return std::abs((a.x * b.y - a.y * b.x) + (b.x * c.y - b.y * c.x) + (c.x * d.y - c.y * d.x) + (d.x * a.y - d.y * a.x)) / 2;
+}
+
+// Function to extract all possible trapezoids from a graph
+std::vector<cv::Vec8i> findTrapezoids(const std::vector<cv::Point2i>& intersections, const std::vector<std::vector<bool>>& adjacencyMatrix, int minArea, int maxArea) 
+{
+    std::vector<cv::Vec8i> trapezoids;
+    for (int i = 0; i < intersections.size(); ++i) {
+        for (int j = i + 1; j < intersections.size(); ++j) {
+            if (!adjacencyMatrix[i][j]) continue;
+
+            for (int k = j + 1; k < intersections.size(); ++k) {
+                if (!adjacencyMatrix[j][k]) continue;
+
+                for (int l = k + 1; l < intersections.size(); ++l) {
+                    if (!adjacencyMatrix[k][l] || !adjacencyMatrix[l][i]) continue;
+                    
+                    int trapezoidArea = trapezoid_area(intersections[i], intersections[j], intersections[k], intersections[l]);
+                    if (trapezoidArea < minArea || trapezoidArea > maxArea) continue;
+
+                    trapezoids.push_back(cv::Vec8i(intersections[i].x, intersections[i].y,
+                                                    intersections[j].x, intersections[j].y,
+                                                    intersections[k].x, intersections[k].y,
+                                                    intersections[l].x, intersections[l].y));
+                }
+            }
+        }
+    }
+    return trapezoids;
+}
+
+cv::Mat reduceSaturationBrightness(const cv::Mat& image, float saturationScale, float valueScale) 
+{
+    cv::Mat hsvImage;
+    cv::cvtColor(image, hsvImage, cv::COLOR_BGR2HSV);
+
+    std::vector<cv::Mat> channels;
+    cv::split(hsvImage, channels);
+
+    channels[1] *= saturationScale;
+    channels[2] *= valueScale;
+
+    cv::merge(channels, hsvImage);
+    cv::Mat result;
+    cv::cvtColor(hsvImage, result, cv::COLOR_HSV2BGR);
+    return result;
 }
